3169fbb3862b855a5e5b80b158f4ff8fbb983f50
Pablo <94941513+pablo-de@users.noreply.github.com>
2023-02-09 18:19:22 -0300

    Create git-with-ssh.sh

---
+++ git-with-ssh.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+current_dir=$(pwd)
+
+if [[ $current_dir == *"work"* ]]; then
+  sed -i 's|IdentityFile ~/.ssh/id_rsa|IdentityFile ~/.ssh/work_rsa|' ~/.ssh/config
+else
+  sed -i 's|IdentityFile ~/.ssh/support-gitlab|IdentityFile ~/.ssh/id_rsa|' ~/.ssh/config
+fi
+
+git "$@"
