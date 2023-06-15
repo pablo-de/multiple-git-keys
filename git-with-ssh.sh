831de90d0ed18369a90049b11381ca1fe1044bf6
Pablo <94941513+pablo-de@users.noreply.github.com>
2023-06-15 17:33:59 -0300

    Update git-with-ssh.sh

--- git-with-ssh.sh
+++ git-with-ssh.sh
@@ -5,7 +5,7 @@ current_dir=$(pwd)
 if [[ $current_dir == *"work"* ]]; then
   sed -i 's|IdentityFile ~/.ssh/id_rsa|IdentityFile ~/.ssh/work_rsa|' ~/.ssh/config
 else
-  sed -i 's|IdentityFile ~/.ssh/support-gitlab|IdentityFile ~/.ssh/id_rsa|' ~/.ssh/config
+  sed -i 's|IdentityFile ~/.ssh/work_rsa|IdentityFile ~/.ssh/id_rsa|' ~/.ssh/config
 fi
 
 git "$@"
